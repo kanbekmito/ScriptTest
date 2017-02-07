f07e4794d66c6249494f2a07998e9c31328cafce
hideyuki arakawa <hideyuki-kreva@hotmail.co.jp>
2017-02-07 20:38:18 +0900

    課題の提出
    Lesson4の課題再提出

---
+++ scriptkadai/Assets/ScriptTest.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptTest : MonoBehaviour {
+
+    // Use this for initialization
+    void Start()
+    {
+        // 配列を初期化する
+        int[] points = { 20, 55, 121, 5, 92, 66, 36, 49 };
+
+        for (int i = 0; i < points.Length; i++)
+        {
+                // 配列の要素を表示する
+                Debug.Log(points[i]);
+        }
+
+        for (int i = 7; i >= 0; i--)
+        {
+            // 配列の要素を表示する
+            Debug.Log(points[i]);
+        }
+        
+        }
+    }
+
+
